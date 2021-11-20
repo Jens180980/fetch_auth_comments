ef743f43db06ad8bff499388e5cbb81ff9d73063
jens harder <jens180980@gmail.com>
2021-11-20 12:15:53 +0100

    access granted :)

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -3,24 +3,48 @@ import {
   apiResult
 } from './helpers.js'
 
+//Listening for sendbutton click to fire main function
+// sendBtn.addEventListener('click', (e) => {
+//   e.preventDefault()
+//   getData(usernameInput.value, passwordInput.value)
+// })
+
 //Main datahandling function
 const getData = async () => {
-  const formData = new FormData()
-  formData.append('username', 'hans')
-  formData.append('password', 'hans')
+  let data
+  // Get token to acces api
+  async function getToken() {
+    const formData = new FormData()
+    formData.append('username', 'admin')
+    formData.append('password', '1234')
 
-  const options = {
-    method: 'POST',
-    body: formData
-  }
+    const options = {
+      method: 'POST',
+      body: formData
+    }
 
-  const data = await apiResult('https://api.mediehuset.net/token', options)
-  sessionStorage.setItem('token', JSON.Stringify(data))
-  const token = JSON.Parse(sessionStorage.getItem('token'))
+    data = await apiResult('https://api.mediehuset.net/token', options)
+    sessionStorage.setItem('authInfo', JSON.stringify(data))
 
-  console.log(token);
+  } // End of getToken
+  getToken()
+
+  // Accessing and retrieving data
+  async function getAccess() {
+    const loginData = JSON.parse(sessionStorage.getItem('authInfo'));
+
+    const options = {
+
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${loginData.access_token}`
+      }
+    }
+
+    const data = await apiResult('https://api.mediehuset.net/sdg/comments', options);
+  } // End of getAccess
+
+  getAccess()
 
 } // End of getData
-
-// Calling getData
 getData()
